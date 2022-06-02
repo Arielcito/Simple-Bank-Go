b39bb58014fae964af41959a07f8408ba0e7342b
Arielcito <serato.arieli@gmail.com>
2022-06-02 09:20:34 -0300

    add api endpoints

--- api/server.go
+++ api/server.go
@@ -3,22 +3,29 @@ package api
 import (
 	db "github.com/Arielcito/simple-bank-go/db/sqlc"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 )
 
 type Server struct{
-	store *db.Store
+	store db.Store
 	router *gin.Engine
 }
 
-func NewServer(store *db.Store)*Server{
-	server := &Server{store :store}
+func NewServer(store db.Store)*Server{
+	server := &Server{store: store}
 	router := gin.Default()
+
+	if v,ok := binding.Validator.Engine().(*validator.Validate);ok{
+		v.RegisterValidation("currency",validCurrency)
+	}
+	router.POST("/users",server.createUser)
 
 	router.POST("/accounts",server.createAccount)
 	router.GET("/accounts/:id",server.getAccount)
 	router.GET("/accounts",server.listAccount)
-	router.PUT("/accounts",server.updateAccount)
-	router.DELETE("/accounts/:id",server.deleteAccount)
+
+	router.POST("/transfer",server.createTransfer)
 
 	server.router = router
 	return server
